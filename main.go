46235c112f58050e55891544c4b0258df5c28c9d
Tyson Trautmann <tysont@gmail.com>
2018-07-14 20:08:23 +0200

    Adding a dumb hello web server.

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"log"
+	"os"
+	"strconv"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	port := 8080
+	if len(os.Args) > 1 {
+		p, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(-1)
+		}
+		port = p
+	}
+
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func helloHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("hello world!"))
 }
